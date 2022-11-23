d32fa467eca017a257dcf7e7a9b9c3db863ab905
Simon Eliasen <simoneliasen@gmail.com>
2022-11-23 15:25:47 +0100

    changed to python2

--- process_data.sh
+++ process_data.sh
@@ -1,4 +1,4 @@
-(cd FB15k && python FB15k_parse.py)
-(cd WN && python WN_parse.py)
-(cd FB15k237 && python FB15k237_parse.py)
-(cd WN18RR && python WN18RR_parse.py)
+(cd FB15k && python2 FB15k_parse.py)
+(cd WN && python2 WN_parse.py)
+(cd FB15k237 && python2 FB15k237_parse.py)
+(cd WN18RR && python2 WN18RR_parse.py)
